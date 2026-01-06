d552a7869c172996ac02f44c452072cb25abf682
LAPTOP-1B36OERS\Asus <ngocnthse184581@fpt.edu.vn>
2026-01-06 23:16:33 +0700

    Register UI

---
+++ src/app/(auth)/login/login-form.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import * as React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  // FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+// import {
+//   InputGroup,
+//   InputGroupAddon,
+//   InputGroupText,
+//   InputGroupTextarea,
+// } from "@/components/ui/input-group";
+import envConfig from "@/config";
+
+const formSchema = z.object({
+  email: z.string().refine((val) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val), {
+    message: "Invalid email address.",
+  }),
+
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters.")
+    .max(100, "Password must be at most 100 characters."),
+});
+
+export default function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  // 2. Define a submit handler ( Nơi gắn api)
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      const res = await fetch(
+        `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/auth/login`,
+        {
+          body: JSON.stringify(values),
+          headers: {
+            "Content-Type": "application/json",
+          },
+
+          method: "POST",
+        }
+      ).then(async (res) => {
+        const payload = await res.json();
+        const data = {
+          status: res.status,
+          payload,
+        };
+        if (!res.ok) {
+          throw data;
+        }
+        return data;
+      });
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  }
+  return (
+    <Card className="w-full sm:max-w-md">
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold text-center">
+          Đăng nhập
+        </CardTitle>
+        <CardDescription className="font-bold text-center">
+          Chào mừng bạn quay lại
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="email">Email</FieldLabel>
+                  <Input
+                    {...field}
+                    id="email"
+                    type="email"
+                    placeholder="email@example.com"
+                    autoComplete="email"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <Input
+                    {...field}
+                    id="password"
+                    type="password"
+                    autoComplete="new-password"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Field orientation="horizontal">
+          <Button type="button" variant="outline" onClick={() => form.reset()}>
+            Reset
+          </Button>
+          <Button type="submit" form="form-rhf-demo">
+            Submit
+          </Button>
+        </Field>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ src/config.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+const configSchema = z.object({
+  NEXT_PUBLIC_API_ENDPOINT: z.string(),
+});
+const configProject = configSchema.safeParse({
+  // phải ghi như vậy thì ở server và client mới dùng được còn nếu chỉ ghi env.process thì client ko chạy được
+  NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+});
+if (!configProject.success) {
+  console.error("Invalid environment variables:", configProject.error.issues);
+  throw new Error("Các giá trị khai báo trong file .env không hợp lệ");
+}
+const envConfig = configProject.data;
+export default envConfig;
