f4b0d38270e43da58de7a9ec30681d652db23203
Hưng Trương <hungtqse182075@fpt.edu.vn>
2026-04-08 00:49:15 +0700

    fix: cate resource

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -198,8 +198,8 @@ export async function getProductCategories(params: {
   const useNextApi = shouldUseNextApi(params.options);
   const filter = encodeURIComponent(JSON.stringify({ page, size }));
   const beUrl = useNextApi
-    ? `/api/product/categories?page=${page}&size=${size}`
-    : `${base}/product/categories?filter=${filter}`;
+    ? `/api/categories?page=${page}&size=${size}`
+    : `${base}/categories?page=${page}&size=${size}`;
 
   const res = await fetch(beUrl, {
     method: "GET",
@@ -229,7 +229,7 @@ export async function deleteProductCategoryById(
   accessToken?: string,
 ): Promise<DeleteResponse> {
   const base = envConfig.NEXT_PUBLIC_API_ENDPOINT.replace(/\/$/, "");
-  const beUrl = `${base}/product/categories/${id}`;
+  const beUrl = `${base}/categories/${id}`;
 
   console.log(
     "[deleteProductCategoryById] id:",
@@ -294,7 +294,7 @@ export async function updateProductCategoryById(
   accessToken?: string,
 ) {
   const base = envConfig.NEXT_PUBLIC_API_ENDPOINT.replace(/\/$/, "");
-  const beUrl = `${base}/product/categories/${id}`;
+  const beUrl = `${base}/categories/${id}`;
 
   const res = await fetch(beUrl, {
     method: "PUT",
@@ -356,7 +356,7 @@ export async function createProductCategory(body: {
   accessToken?: string;
 }) {
   const base = envConfig.NEXT_PUBLIC_API_ENDPOINT.replace(/\/$/, "");
-  const beUrl = `${base}/product/categories`;
+  const beUrl = `${base}/categories`;
 
   const res = await fetch(beUrl, {
     method: "POST",
@@ -423,7 +423,7 @@ export async function getCategoryById(
   accessToken?: string,
 ): Promise<ProductCategoriesResponse> {
   const base = envConfig.NEXT_PUBLIC_API_ENDPOINT.replace(/\/$/, "");
-  const beUrl = `${base}/product/categories/${id}`;
+  const beUrl = `${base}/categories/${id}`;
 
   const res = await fetch(beUrl, {
     method: "GET",
